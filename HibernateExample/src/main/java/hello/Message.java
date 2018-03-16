5e7134afe5f8b6e3929aebf04a7fb269965f92a3
ChiseBogdan <chise_b@yahoo.com>
2018-03-16 11:13:45 +0200

    HibernateConfiguration

---
+++ HibernateExample/src/main/java/hello/Message.java
@@ -0,0 +1,20 @@
+package hello;
+
+/**
+ * Created by grigo on 4/23/17.
+ */
+public class Message {
+    private Long id;
+    private String text;
+    private Message nextMessage;
+    public Message() {}
+    public Message(String text) {  this.text = text;   }
+    public Long getId() { return id;  }
+    private void setId(Long id) { this.id = id; }
+    public String getText() { return text; }
+    public void setText(String text) { this.text = text;  }
+    public Message getNextMessage() {  return nextMessage; }
+    public void setNextMessage(Message nextMessage) {
+        this.nextMessage = nextMessage;
+    }
+}
