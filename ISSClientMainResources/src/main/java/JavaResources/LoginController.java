99b2d5ea7f4777ee52adc1e6874b0b26a0a6ace5
otnielboros <oti_otniel97@yahoo.com>
2018-04-21 20:07:10 +0300

    Clients.Run

---
+++ ISSClientMainResources/src/main/java/JavaResources/LoginController.java
@@ -0,0 +1,12 @@
+package JavaResources;
+
+import JavaResources.Service.Service;
+import JavaResources.View.Loader;
+import JavaResources.View.StageManager;
+
+public class LoginController implements Controller {
+    @Override
+    public void initialize(StageManager stageManager, Service service, Loader loader) {
+
+    }
+}
