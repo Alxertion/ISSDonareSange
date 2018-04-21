46b1af5f1762c238373b0905a4d4d93ae7c54134
ivanpaul7 <ivanpaul7@yahoo.com>
2018-04-21 17:28:38 +0300

    Paul::: Module v2

--- ISSClientMainResources/src/main/java/JavaResources/View/FXMLEnum.java
+++ ISSClientMainResources/src/main/java/JavaResources/View/FXMLEnum.java
@@ -1,71 +1,71 @@
-package JavaResources.View;
-
-public enum FXMLEnum {
-
-    MainWindowDonator {
-        @Override
-        public String getFxmlFile() {
-            return "/FXMLDonator/MainWindowDonator.fxml";
-        }
-
-        @Override
-        public String getTitle() {
-            return "MainWindowDonator";
-        }
-
-    },
-    ConditiiDonare{
-        @Override
-        public String getFxmlFile() {
-            return "/FXMLDonator/ConditiiDonare.fxml";
-        }
-
-        @Override
-        public String getTitle() {
-            return "ConditiiDonare";
-        }
-    },
-    FormularDonator2{
-        @Override
-        public String getFxmlFile(){return "/FXMLPersonalTransfuzii/JavaResources.FormularDonator.fxml";}
-
-        @Override
-        public String getTitle(){return "JavaResources.FormularDonator";}
-    },
-    MainViewPersonalTransfuzii{
-        @Override
-        public String getFxmlFile(){return "/FXMLPersonalTransfuzii/PersonalTransfuzii.fxml";}
-
-        @Override
-        public String getTitle(){return "PersonalTransfuzii";}
-    },
-    TrimiteMesaj {
-        @Override
-        public String getFxmlFile() {
-            return "/FXMLPersonalTransfuzii/Mesaj.fxml";
-        }
-
-        @Override
-        public String getTitle() {
-            return "TrimitereMesaj";
-        }
-    },
-    FormularDonator{
-        @Override
-        public String getFxmlFile() {
-            return "/FXMLDonator/JavaResources.FormularDonator.fxml";
-        }
-
-        @Override
-        public String getTitle() {
-            return "JavaResources.FormularDonator";
-        }
-    };
-
-
-
-    public abstract String getFxmlFile();
-
-    public abstract String getTitle();
-
-}
+//package JavaResources.View;
+//
+//public enum FXMLEnum {
+//
+//    MainWindowDonator {
+//        @Override
+//        public String getFxmlFile() {
+//            return "/FXMLDonator/MainWindowDonator.fxml";
+//        }
+//
+//        @Override
+//        public String getTitle() {
+//            return "MainWindowDonator";
+//        }
+//
+//    },
+//    ConditiiDonare{
+//        @Override
+//        public String getFxmlFile() {
+//            return "/FXMLDonator/ConditiiDonare.fxml";
+//        }
+//
+//        @Override
+//        public String getTitle() {
+//            return "ConditiiDonare";
+//        }
+//    },
+//    FormularDonator2{
+//        @Override
+//        public String getFxmlFile(){return "/FXMLPersonalTransfuzii/JavaResources.FormularDonator.fxml";}
+//
+//        @Override
+//        public String getTitle(){return "JavaResources.FormularDonator";}
+//    },
+//    MainViewPersonalTransfuzii{
+//        @Override
+//        public String getFxmlFile(){return "/FXMLPersonalTransfuzii/PersonalTransfuzii.fxml";}
+//
+//        @Override
+//        public String getTitle(){return "PersonalTransfuzii";}
+//    },
+//    TrimiteMesaj {
+//        @Override
+//        public String getFxmlFile() {
+//            return "/FXMLPersonalTransfuzii/Mesaj.fxml";
+//        }
+//
+//        @Override
+//        public String getTitle() {
+//            return "TrimitereMesaj";
+//        }
+//    },
+//    FormularDonator{
+//        @Override
+//        public String getFxmlFile() {
+//            return "/FXMLDonator/JavaResources.FormularDonator.fxml";
+//        }
+//
+//        @Override
+//        public String getTitle() {
+//            return "JavaResources.FormularDonator";
+//        }
+//    };
+//
+//
+//
+//    public abstract String getFxmlFile();
+//
+//    public abstract String getTitle();
+//
+//}
