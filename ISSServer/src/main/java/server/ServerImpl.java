f4352f3dd9a7e66ac77d3b71ecdd6cb44806a78b
otnielboros <oti_otniel97@yahoo.com>
2018-05-05 14:42:39 +0300

    My Push Otniel:))

--- ISSServer/src/main/java/server/ServerImpl.java
+++ ISSServer/src/main/java/server/ServerImpl.java
@@ -120,12 +120,12 @@ public class ServerImpl implements IServices {
         final String mailPassword="alexertion";
         Properties props=new Properties();
         props.put("mail.smtp.user", user);
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "465");
         props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.port", "465");
         props.put("mail.smtp.debug", "true");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.socketFactory.port", "465");
+        props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
         props.put("mail.smtp.socketFactory.fallback", "false");
         javax.mail.Session sessionGmail;
