825ef51f85bb0532e786a85a29e9d8216c37fb50
otnielboros <oti_otniel97@yahoo.com>
2018-05-05 19:21:16 +0300

    Update again

--- ISSModel/src/main/java/model/Persoana.java
+++ ISSModel/src/main/java/model/Persoana.java
@@ -11,7 +11,7 @@ public class Persoana implements Serializable{
      * Default constructor
      */
     public Persoana() {
-        System.out.println("Ceva");
+        System.out.println("Cevllla");
     }
 
     /**
