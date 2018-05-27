f71bc9710c3fd79014c4723cd4e3282f851fe5b8
otnielboros <oti_otniel97@yahoo.com>
2018-05-27 21:21:32 +0300

    Merge cu Oti :D

--- ISSServer/src/main/java/StartServer.java
+++ ISSServer/src/main/java/StartServer.java
@@ -5,5 +5,6 @@ public class StartServer {
 
     public static void main(String[] args){
         ApplicationContext factory = new ClassPathXmlApplicationContext("classpath:server-spring.xml");
+        System.out.println("Waiting for clients...(Paul :D)");
     }
 }
